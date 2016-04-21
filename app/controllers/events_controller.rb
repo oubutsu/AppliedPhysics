9c0ccff145b0f61f6e13e341639178c17643b5a5
nanopoteto <nano.poteto@gmail.com>
2016-04-21 16:19:51 +0900

    hello

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :deadline, :place, :comment)
+      params.require(:event).permit(:name, :deadline, :place, :comment, :info)
     end
 end
